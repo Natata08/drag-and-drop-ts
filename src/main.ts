6058a98e0bd353876d3772ca84dd6fab9ea0ce70
Natalia Lapina <lapinann08@gmail.com>
2025-07-10 16:53:14 +0200

    Initial commit: Drag and drop project setup with Vite and TypeScript

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import './style.css';
+
+console.log('Project starting...');
+
+// Basic initialization
+const appElement = document.getElementById('app');
+if (appElement) {
+  console.log('App element found, project ready to be set up.');
+}
