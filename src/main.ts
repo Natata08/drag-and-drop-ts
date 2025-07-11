db7f308bf4f4c01947637f2bf599a6b9b0a0932f
Natalia Lapina <lapinann08@gmail.com>
2025-07-11 20:22:31 +0200

    add OOP class for ProjectInput

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,24 @@
-import './style.css';
+import './style.css'
 
-console.log('Project starting...');
+class ProjectInput {
+  templateElement: HTMLTemplateElement
+  hostElement: HTMLDivElement
+  element: HTMLFormElement
 
-// Basic initialization
-const appElement = document.getElementById('app');
-if (appElement) {
-  console.log('App element found, project ready to be set up.');
+  constructor() {
+    this.templateElement = document.getElementById(
+      'project-input'
+    )! as HTMLTemplateElement
+    this.hostElement = document.getElementById('app')! as HTMLDivElement
+
+    const importedNode = document.importNode(this.templateElement.content, true)
+    this.element = importedNode.firstElementChild as HTMLFormElement
+    this.attach()
+  }
+
+  private attach() {
+    this.hostElement.insertAdjacentElement('afterbegin', this.element)
+  }
 }
+
+const prjInput = new ProjectInput()
