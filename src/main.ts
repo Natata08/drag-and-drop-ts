176b977184f5e95fc3c197f4d565b6c09f9ea217
Natalia Lapina <lapinann08@gmail.com>
2025-07-22 09:56:43 +0200

    refactor: move validation logic to separate utility module

--- src/main.ts
+++ src/main.ts
@@ -2,56 +2,8 @@ import './style.css'
 import type { DragTarget, Draggable } from './models/drag-drop-interfaces.js'
 import { Project, ProjectStatus } from './models/project.js'
 import { projectState } from './state/project-state.js'
-
-//validation
-interface Validatable {
-  value: string | number
-  required?: boolean
-  minLength?: number
-  maxLength?: number
-  min?: number
-  max?: number
-}
-
-function validate(validatableInput: Validatable) {
-  let isValid = true
-
-  if (validatableInput.required) {
-    isValid = isValid && validatableInput.value.toString().trim().length !== 0
-  }
-
-  if (
-    validatableInput.minLength != null &&
-    typeof validatableInput.value === 'string'
-  ) {
-    isValid =
-      isValid && validatableInput.value.length >= validatableInput.minLength
-  }
-
-  if (
-    validatableInput.maxLength != null &&
-    typeof validatableInput.value === 'string'
-  ) {
-    isValid =
-      isValid && validatableInput.value.length <= validatableInput.maxLength
-  }
-
-  if (
-    validatableInput.min != null &&
-    typeof validatableInput.value === 'number'
-  ) {
-    isValid = isValid && validatableInput.value >= validatableInput.min
-  }
-
-  if (
-    validatableInput.max != null &&
-    typeof validatableInput.value === 'number'
-  ) {
-    isValid = isValid && validatableInput.value <= validatableInput.max
-  }
-
-  return isValid
-}
+import type { Validatable } from './util/validation.js'
+import { validate } from './util/validation.js'
 
 //autobind decorator
 function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
